fc24fc697891bab3ea7e23ba6fbe6cc4dc2a0826
hakan-gleissman <hakan.gleissman@gmail.com>
2025-09-09 09:15:21 +0200

    Ändrade i main

--- src/main/java/se/sprinto/hakan/project1/Main.java
+++ src/main/java/se/sprinto/hakan/project1/Main.java
@@ -1,4 +1,9 @@
 package se.sprinto.hakan.project1;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        System.out.println("Goodbye!");
+    }
 }
